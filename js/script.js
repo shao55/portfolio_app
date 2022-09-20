73aa100a2b60ee13a50eddc375f794832dbc8c23
Nurgissa <nurgissa.22@gmail.com>
2022-09-20 15:15:50 +0600

    Day 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
+// Массив с фразами
 const phrases = [
+    "Нургиса",
     "начинающий FrontEnd разработчик",
     "автолюбитель",
     "фотограф - любитель"
 ];
 
+// Дотягиваюсь к фразе на странице
+let phrase = document.querySelector(".phrase");
+
+// Функция - рандомный текст
+function getRandomElement (arr) {
+    let randIndex = Math.floor(Math.random()*arr.length);
+    return(arr[randIndex]);
+};
+
+// Кладем рандомную фразу в переменную
+let randomElement = getRandomElement(phrases);
+
+// Заменяю текст на странице рандомным элементом
+// phrase.textContent = getRandomElement(phrases);
+
+for (let i = 0; i <= 10; i = i + 1) {
+    smoothly(phrase, "textContent", randomElement[i]);
+}
